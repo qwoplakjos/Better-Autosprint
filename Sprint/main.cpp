e824fded8192e9af6a222de51f6fd1769c7e9699
Федотов Михаил <qwoplakjos@gmail.com>
2023-02-23 17:24:59 +0300

    a

--- Sprint/main.cpp
+++ Sprint/main.cpp
@@ -4,11 +4,64 @@
 #include <string>
 #include <thread>
 
+
+int sprint = 0;
+int forward = 0;
+bool sprinting = false;
+
+
+void Sprint() {
+	INPUT ip[2] = { 0, 0 };
+	ip[0].type = INPUT_KEYBOARD; ip[0].ki.wVk = sprint;
+	ip[1].type = INPUT_KEYBOARD; ip[1].ki.wVk = sprint;
+	ip[1].ki.dwFlags = KEYEVENTF_KEYUP;
+
+	while (true) {
+		if (sprinting && GetForegroundWindow() == FindWindow(NULL, "Minecraft")) {
+			CURSORINFO cursor = { sizeof(cursor) };
+			if (GetCursorInfo(&cursor)) {
+				if (cursor.flags == 0)
+				{
+					SendInput(1, &ip[0], sizeof(INPUT));
+					std::this_thread::sleep_for(std::chrono::milliseconds(50));
+					SendInput(1, &ip[1], sizeof(INPUT));
+					sprinting = false;
+				}
+			}
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	}
+}
+
+LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (nCode >= 0)
+	{
+		if (wParam == WM_KEYDOWN)
+		{
+			PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
+			if (p->vkCode == forward && !sprinting)
+			{
+				sprinting = true;
+			}
+		}
+		else if (wParam == WM_KEYUP)
+		{
+			PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam;
+			if (p->vkCode == forward && sprinting)
+			{
+				sprinting = false;
+			}
+		}
+	}
+	return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+
+
+
+
 int main()
 {
-	CreateMutexA(0, FALSE, "SPRINTMOD");
-	if (GetLastError() == ERROR_ALREADY_EXISTS) return 0;
-	FreeConsole();
 
 	char* buf = nullptr;
 	size_t sz = 0, pos = 0;
@@ -26,11 +79,9 @@ int main()
 	std::string del = ":", key;
 	std::ifstream options(path + "\\options.txt");
 
-	int forward = 0, sprint = 0, delay = 250;
-
 	if (options.is_open()) {
 		while (getline(options, key))
-		{		
+		{
 			if (key.find("keyboard_type_0_key.forward") != std::string::npos) {
 				pos = key.find(del) + 1;
 				forward = stoi(key.substr(pos));
@@ -45,26 +96,20 @@ int main()
 	}
 	else return 0;
 
-	INPUT ip[2] = { 0, 0 };
-	ip[0].type = INPUT_KEYBOARD; ip[0].ki.wVk = sprint;
-	ip[1].type = INPUT_KEYBOARD; ip[1].ki.wVk = sprint;
-	ip[1].ki.dwFlags = KEYEVENTF_KEYUP;
+	std::cout << "Sprint key found: " << (char)sprint;
 
-	do {
-		if (GetForegroundWindow() == FindWindow(NULL, "Minecraft"))
-		{
-			CURSORINFO cursor = { sizeof(cursor) };
-			if (GetCursorInfo(&cursor)) {
-				if (cursor.flags == 0 && GetAsyncKeyState(forward))
-				{
-					SendInput(1, &ip[0], sizeof(INPUT));
-					std::this_thread::sleep_for(std::chrono::milliseconds(delay / 2));
-					SendInput(1, &ip[1], sizeof(INPUT));
-				}
-			}
-		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
-	} while (true);
+	std::thread a(Sprint);
+
+	HHOOK hhkLowLevelKybd = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, 0, 0);
+
+	MSG msg;
+	while (!GetMessage(&msg, NULL, NULL, NULL)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	UnhookWindowsHookEx(hhkLowLevelKybd);
+
 
 	return 0;
 }
